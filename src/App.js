eabde78a81b2369e9f94bdc3432d419b4fa383f3
Mike <jones@cs.byu.edu>
2023-10-12 10:38:46 -0600

    initial commit local state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+function Square() {
+  // useState is a hook that returns two values. 
+  // the first, which I'm storing in "value"
+  // is a variable that holds the state. 
+  // the second, which I'm storing in setValue
+  // is a function that sets the value of the state. 
+  // when I call setValue and give it a new value 
+  // react also forces an update and redraw. 
+  const [value, setValue] = useState(null);
+  function handleClick() {
+    // a simple call to setValue. 
+    setValue("X");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
   );
 }
-
-export default App;
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+}
